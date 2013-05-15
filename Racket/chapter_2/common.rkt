84968b3ac3b2636227012833bfd0967fb8af949e
Rubaka <r11baka@gmail.com>
2013-05-15 11:15:14 +0300

    add gitignore

--- Racket/chapter_2/common.rkt
+++ Racket/chapter_2/common.rkt
@@ -1,19 +1,22 @@
 (define nil '())   
+;filter
  (define (filter predicate sequence) 
    (cond ((null? sequence) nil) 
          ((predicate (car sequence)) 
           (cons (car sequence)  
                 (filter predicate (cdr sequence)))) 
-         (else (filter predicate (cdr sequence))))) 
+         (else (filter predicate (cdr sequence)))))
+ ;accumulate
    (define (accumulate op initial sequence) 
    (if (null? sequence) 
        initial 
        (op (car sequence) 
            (accumulate op initial (cdr sequence))))) 
+   ;ebumerate interval
    (define (enumerate-interval low high) 
    (if (> low high) 
        nil 
        (cons low (enumerate-interval (+ low 1) high)))) 
-  
+  ;flatmap
  (define (flatmap proc seq) 
    (accumulate append nil (map proc seq)))
