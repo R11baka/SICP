bb0aa3d6c3349ae2edc6ec35c6103621eba221af
Rubaka <r11baka@gmail.com>
2013-05-16 10:31:26 +0300

    explaing fo queens size

--- Racket/chapter_2/2.42.rkt
+++ Racket/chapter_2/2.42.rkt
@@ -1,5 +1,32 @@
 ;common defines
 (load "common.rkt")
+;
+(define empty-board nil) 
+; postions
+(define (make-position row col)
+  (cons row col))
+(define (get-row position)
+  (car position) )
+(define (get-col position)(cdr position))
+;
+(define (adjoin-position row col queens)
+  (append queens (list (make-position row col))))
+
+(define (safe? col positions)
+   (let ((kth-queen (list-ref positions (- col 1)))
+         (other-queens (filter (lambda (q)
+                                 (not (= col (get-col q))))
+                               positions)))
+   (define (attacks? q1 q2)
+     (or (= (get-row q1) (get-row q2))
+         (= (abs (- (get-row q1) (get-row q2)))
+            (abs (- (get-col q1) (get-col q2))))))
+
+   (define (iter q board)
+     (or (null? board)
+         (and (not (attacks? q (car board)))
+              (iter q (cdr board)))))
+   (iter kth-queen other-queens)))
 ;main
 (define (queens board-size)
    (define (queen-cols k) 
@@ -14,3 +41,4 @@
                   (enumerate-interval 1 board-size)))
            (queen-cols (- k 1))))))
    (queen-cols board-size))
+(queens 4)
