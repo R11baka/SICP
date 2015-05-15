c5680fa9fcf4c9266cc20e8e3dd4322c5c66b541
Alex.V <a.voloschenko@appsorama.com>
2015-05-15 18:00:40 +0300

    add deriv package

---
+++ Racket/chapter_2/generic_operations/derivPackage.rkt
@@ -0,0 +1,72 @@
+#lang planet neil/sicp
+(define global-array '())
+
+(define (make-entry k v) (list k v))
+(define (key entry) (car entry))
+(define (value entry) (cadr entry))
+
+(define (put op type item)
+  (define (put-helper k array)
+    (cond ((null? array) (list(make-entry k item)))
+          ((equal? (key (car array)) k) array)
+          (else (cons (car array) (put-helper k (cdr array))))))
+  (set! global-array (put-helper (list op type) global-array)))
+
+(define (get op type)
+  (define (get-helper k array)
+    (cond ((null? array) #f)
+          ((equal? (key (car array)) k) (value (car array)))
+          (else (get-helper k (cdr array)))))
+  (get-helper (list op type) global-array))
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+(and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product m1 m2) (list '* m1 m2))
+(define (sum? x)
+(and (pair? x) (eq? (car x) '+)))
+(define (product? x)(eq? (car x) '*))
+(define (addend s) (cadr s))
+(define (augend s) (caddr s))
+(define (deriv exp var) (cond ((number? exp) 0)
+((variable? exp) (if (same-variable? exp var) 1 0)) ((sum? exp)
+(make-sum (deriv (addend exp) var) (deriv (augend exp) var)))
+(else (error "неизвестный тип выражения -- DERIV" exp))))
+
+(define (operator exp) (car exp)) 
+(define (operands exp) (cdr exp))
+
+(define (deriv1 exp var)
+   (cond ((number? exp) 0)
+((variable? exp) (if (same-variable? exp var) 1 0)) (else ((get 'deriv (operator exp)) (operands exp)
+var))))
+
+(define (sum-deriv operands var)
+    (make-sum
+     (deriv1 (car operands) var)
+     (deriv1 (cadr operands) var)
+    ))
+> 
+
+> (define (install-sum-deriv)
+     (define (sum-deriv operands var)
+    (make-sum
+     (deriv1 (car operands) var)
+     (deriv1 (cadr operands) var)
+    ))
+    (define (sum-deriv1 operands var)
+      (cond 
+        ((null? operands) 0)
+        (else 
+      (make-sum
+       (deriv1 (car operands) var)
+       (sum-deriv1 (cdr operands) var)
+      ))
+    ))
+    
+    (put 'deriv '+ sum-deriv1)    
+    'done)
+> (install-sum-deriv)
+
+>
